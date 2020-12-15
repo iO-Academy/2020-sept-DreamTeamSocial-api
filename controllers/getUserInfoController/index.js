441eebf7748a3e28721e4e98ed0a2d304e9cfb47
Ellie <erobinson98@btinternet.com>
2020-12-15 13:42:18 +0000

    remove console.log

--- controllers/getUserInfoController/index.js
+++ controllers/getUserInfoController/index.js
@@ -4,7 +4,6 @@ const getDbConnection = require("../../dbConnection");
 async function getUserInfo(req, res) {
     getDbConnection();
     User.findOne({username: req.params.user}, (err, user) => {
-        console.log(user)
         return res.json({success: true, message: 'got user info', info: {username: user.username,  bio: user.bio || ''}});
     } )
 
