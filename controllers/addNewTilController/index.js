08a5d522089dd2841322589745448ff02e02cf52
El Stevens <eleanorkwstevens@gmail.com>
2020-12-16 09:24:03 +0000

    added new font, made sure only getting data if a user is logged in

--- controllers/addNewTilController/index.js
+++ controllers/addNewTilController/index.js
@@ -2,15 +2,18 @@ const Til = require('../../models/til.js');
 const getDbConnection = require("../../dbConnection");
 
 async function addNewTil(req, res) {
-    getDbConnection();
-    // const Til = new Til;
-    Til.create({username: req.body.username, tilPost: req.body.tilPost}, async (err, Til) => {
-        if (err) {
-            return res.status(500).json({success: false, message: 'error', info: ''});
-        } else {
-            return res.json({success: true, message: 'got til post info', info: ''});
-        }
-    });
+    if (req.user) {
+        getDbConnection();
+        Til.create({username: req.body.username, tilPost: req.body.tilPost}, async (err, Til) => {
+            if (err) {
+                return res.status(500).json({success: false, message: 'error', info: ''});
+            } else {
+                return res.json({success: true, message: 'got til post info', info: ''});
+            }
+        });
+    } else {
+        return res.status(400).json({success: false, message: 'user not logged in', info: ''});
+    }
 }
 
 module.exports = addNewTil;
--- controllers/getTilsController/index.js
+++ controllers/getTilsController/index.js
@@ -2,15 +2,18 @@ const Til = require('../../models/til.js');
 const getDbConnection = require("../../dbConnection");
 
 async function getTils(req, res) {
-    getDbConnection();
-    Til.find({username: req.params.user}, (err, tils) => {
-        if (err) {
-            return res.status(500).json({success: false, message: 'error', info: ''});
-        } else {
-            return res.json({success: true, message: 'got til info', info: tils});
-        }
-    });
-
+    if (req.user) {
+        getDbConnection();
+        Til.find({username: req.params.user}, (err, tils) => {
+            if (err) {
+                return res.status(500).json({success: false, message: 'error', info: ''});
+            } else {
+                return res.json({success: true, message: 'got til info', info: tils});
+            }
+        });
+    } else {
+        return res.status(400).json({success: false, message: 'user not logged in', info: ''});
+    }
 }
 
 module.exports = getTils;
